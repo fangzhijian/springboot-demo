0420248da91380ab0fb38024db26f5917fa0790b
fangzhijian <657833870@qq.com>
2025-12-31 09:51:31 +0800

    提交

--- src/main/java/com/springboot/demo/config/WebClientConfig.java
+++ src/main/java/com/springboot/demo/config/WebClientConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
@@ -52,6 +53,8 @@ public class WebClientConfig {
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) // 默认请求头
+                .exchangeStrategies(ExchangeStrategies.builder().codecs(configurer ->
+                        configurer.defaultCodecs().maxInMemorySize(30*1024 * 1024)).build())
                 .build();
     }
 }
